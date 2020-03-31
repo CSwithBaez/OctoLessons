5d05800228f3b81071aabcc1380d6c09153cbd55
ARcheroftheseAS <62357886+ARcheroftheseAS@users.noreply.github.com>
2020-03-31 14:26:14 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var count = 0;
 (function(){
 	var SIID = setInterval(function(){
 		var HEX = Number(Math.round(Math.abs(Math.sin(count))*999999));
-		body.style.backgroundColor = "#" + HEX;
+		bg.style.backgroundColor = "#" + HEX;
 		count += 0.01;
 	}, 200)
 })();
