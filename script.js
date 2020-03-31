c0b13b89467384adf3ddcf859b0d8fdf3ee9aa87
ARcheroftheseAS <62357886+ARcheroftheseAS@users.noreply.github.com>
2020-03-31 15:30:15 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var count = 0.0;
 (function(){
 	var SIID = setInterval(function(){
-		var tweak = Number(Math.sin(count)*4);
+		var tweak = Number(Math.round(Math.sin(count)*4));
 		count += 0.01;
 		document.getElementsByTagName('body')[0].style.backgroundColor = "#" + (151315 + (tweak * 10101));
 	}, 20)
