22fb44d5900c450821c9089910d92f1fb91bd472
ARcheroftheseAS <62357886+ARcheroftheseAS@users.noreply.github.com>
2020-03-31 15:04:11 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ var bg = document.getElementsByTagName('body')[0];
 var count = 0.0;
 var tweak = 0;
 var tweakColor = function(){
-	var SIID = setInterval(function(){
-		tweak = Number(Math.round(Math.sin(count)*4));
+	setInterval(function(){
+		tweak = Number(Math.round(Math.sin(count)*10));
 		bg.style.backgroundColor = 
 			"rgb(" + (65 + tweak).toString()
 			+ "," + (60 + tweak).toString() 
