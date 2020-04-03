ee8e31adb264c66e9a7fbfa2cab9a412b132d9c7
ARcheroftheseAS <62357886+ARcheroftheseAS@users.noreply.github.com>
2020-04-03 10:58:25 -0700

    Update ARcher_HOME.js

--- ARcher_HOME.js
+++ ARcher_HOME.js
@@ -99,18 +99,11 @@ SFToMain.click(function(){
 	animateMain();
 });
 
-var navBut = [];
 var hashes = ["top", "intro", "quetegustahacer", "", "", "communityLinks"];
-for (var i = 0; i < 6; i++){
+for (let i = 0; i < 6; i++){
 	var butLoc = '#' + (i + 1);
-	navBut.push($(butLoc));
+	$(butLoc).click(function(){location.hash = hashes[i]});
 }
-navBut[0].click(function(){location.hash = hashes[0]});
-navBut[1].click(function(){location.hash = hashes[1]});
-navBut[2].click(function(){location.hash = hashes[2]});
-navBut[3].click(function(){location.hash = hashes[3]});
-navBut[4].click(function(){location.hash = hashes[4]});
-navBut[5].click(function(){location.hash = hashes[5]});
 
 toggleNav.click(function(){
 	$(".fas")[0].classList.toggle('fa-angle-double-left');
