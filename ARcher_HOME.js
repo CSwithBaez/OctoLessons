58955740e4a53a61961f39a1367b32f7b4686a14
ARcheroftheseAS <62357886+ARcheroftheseAS@users.noreply.github.com>
2020-04-02 20:48:21 -0700

    Update ARcher_HOME.js

--- ARcher_HOME.js
+++ ARcher_HOME.js
@@ -103,11 +103,15 @@ toggleNav.click(function(){
 	$(".fas").classList.toggle('fa-angle-double-right');
 	if (navState) {
 		$( "iframe" ).animate({width: 15%}, 500);
+		iframe.css('min-width', '120px');
 		toggleNav.animate({left: 16%}, 500);
+		navState != navState;
 	}
 	else {
 		$( "iframe" ).animate({width: 0}, 500);
+		iframe.css('min-width', '0px');
 		toggleNav.animate({left: 1%}, 500);
+		navState != navState;
 	} 
 });
 
