c5c4348495530881b607b284a9e5aed4b617ad62
ARcheroftheseAS <62357886+ARcheroftheseAS@users.noreply.github.com>
2020-04-02 19:47:03 -0700

    Create ARcher_NAV.js

---
+++ ARcher_NAV.js
@@ -0,0 +1,24 @@
+var linkToTop = $('#1');
+var linkToIntro = $('#2');
+var linkToHacer = $('#3');
+var linkToFav = $('#4');
+var linkToChieve = $('#5');
+var linkToComm = $('#6');
+linkToTop.click(function(){
+	window.location = "#top";
+});
+linkToIntro.click(function(){
+	window.location = "#intro";
+});
+linkToHacer.click(function(){
+	window.location = "#quetegustahacer";
+});
+linkToFav.click(function(){
+	window.location = "#";
+});
+linkToChieve.click(function(){
+	window.location = "#";
+});
+linkToComm.click(function(){
+	window.location = "#communityLinks";
+});
