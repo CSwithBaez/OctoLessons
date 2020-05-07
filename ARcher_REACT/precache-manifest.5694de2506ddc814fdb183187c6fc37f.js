9cb259e01e1f3f3b4aa722d86bfcbafca0540750
ARcheroftheseAS <62357886+ARcheroftheseAS@users.noreply.github.com>
2020-05-07 14:10:47 -0700

    Add files via upload

---
+++ ARcher_REACT/precache-manifest.5694de2506ddc814fdb183187c6fc37f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0119ede8ae991d55d77a9a6f4bdd0461",
+    "url": "/OctoLessons/index.html"
+  },
+  {
+    "revision": "8ab19cf7a98fe8f3520c",
+    "url": "/OctoLessons/static/css/main.d81fccab.chunk.css"
+  },
+  {
+    "revision": "7bd32b0d0be5e2930973",
+    "url": "/OctoLessons/static/js/2.5dc8107f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/OctoLessons/static/js/2.5dc8107f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ab19cf7a98fe8f3520c",
+    "url": "/OctoLessons/static/js/main.617763b2.chunk.js"
+  },
+  {
+    "revision": "4815752a094c5bae4437",
+    "url": "/OctoLessons/static/js/runtime-main.27e6611c.js"
+  },
+  {
+    "revision": "56d1fa8e27c79d2380f64a45f6c5a174",
+    "url": "/OctoLessons/static/media/CyberGestalt.56d1fa8e.png"
+  },
+  {
+    "revision": "f3c1d11075b108e9a9b6d5b00705be9d",
+    "url": "/OctoLessons/static/media/Meme.f3c1d110.png"
+  },
+  {
+    "revision": "ba61eccde2eb63c3a61fa20b7fae4afd",
+    "url": "/OctoLessons/static/media/doodleKindness.ba61eccd.png"
+  },
+  {
+    "revision": "312390546ddd5e3dac64c4464d8429f3",
+    "url": "/OctoLessons/static/media/granadagbox.31239054.png"
+  },
+  {
+    "revision": "958904a89892f83b708953429c76334d",
+    "url": "/OctoLessons/static/media/iconB.958904a8.gif"
+  },
+  {
+    "revision": "b6f1c786dfcca8f50a1adbb9c7a856c8",
+    "url": "/OctoLessons/static/media/iconC.b6f1c786.gif"
+  },
+  {
+    "revision": "b66b5dd9e67c1456c5040c3c7c47cb72",
+    "url": "/OctoLessons/static/media/iconD.b66b5dd9.gif"
+  },
+  {
+    "revision": "d7dbe385bd20ee967ed696d6ff984461",
+    "url": "/OctoLessons/static/media/iconE.d7dbe385.gif"
+  },
+  {
+    "revision": "76eb56f0face25dbec10e4e64470ac8f",
+    "url": "/OctoLessons/static/media/iconF.76eb56f0.gif"
+  }
+]);
---
+++ ARcher_REACT/precache-manifest.ee3b3d0c3dfa918a61874488df42d478.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2045d7dc9f9f99c52c82a1f7367415d6",
+    "url": "/OctoLessons/ARcher_REACT/index.html"
+  },
+  {
+    "revision": "8ab19cf7a98fe8f3520c",
+    "url": "/OctoLessons/ARcher_REACT/static/css/main.d81fccab.chunk.css"
+  },
+  {
+    "revision": "7bd32b0d0be5e2930973",
+    "url": "/OctoLessons/ARcher_REACT/static/js/2.5dc8107f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/OctoLessons/ARcher_REACT/static/js/2.5dc8107f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ab19cf7a98fe8f3520c",
+    "url": "/OctoLessons/ARcher_REACT/static/js/main.617763b2.chunk.js"
+  },
+  {
+    "revision": "fba4d807c511e3940597",
+    "url": "/OctoLessons/ARcher_REACT/static/js/runtime-main.8cde2b44.js"
+  },
+  {
+    "revision": "56d1fa8e27c79d2380f64a45f6c5a174",
+    "url": "/OctoLessons/ARcher_REACT/static/media/CyberGestalt.56d1fa8e.png"
+  },
+  {
+    "revision": "f3c1d11075b108e9a9b6d5b00705be9d",
+    "url": "/OctoLessons/ARcher_REACT/static/media/Meme.f3c1d110.png"
+  },
+  {
+    "revision": "ba61eccde2eb63c3a61fa20b7fae4afd",
+    "url": "/OctoLessons/ARcher_REACT/static/media/doodleKindness.ba61eccd.png"
+  },
+  {
+    "revision": "312390546ddd5e3dac64c4464d8429f3",
+    "url": "/OctoLessons/ARcher_REACT/static/media/granadagbox.31239054.png"
+  },
+  {
+    "revision": "958904a89892f83b708953429c76334d",
+    "url": "/OctoLessons/ARcher_REACT/static/media/iconB.958904a8.gif"
+  },
+  {
+    "revision": "b6f1c786dfcca8f50a1adbb9c7a856c8",
+    "url": "/OctoLessons/ARcher_REACT/static/media/iconC.b6f1c786.gif"
+  },
+  {
+    "revision": "b66b5dd9e67c1456c5040c3c7c47cb72",
+    "url": "/OctoLessons/ARcher_REACT/static/media/iconD.b66b5dd9.gif"
+  },
+  {
+    "revision": "d7dbe385bd20ee967ed696d6ff984461",
+    "url": "/OctoLessons/ARcher_REACT/static/media/iconE.d7dbe385.gif"
+  },
+  {
+    "revision": "76eb56f0face25dbec10e4e64470ac8f",
+    "url": "/OctoLessons/ARcher_REACT/static/media/iconF.76eb56f0.gif"
+  }
+]);
